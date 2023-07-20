4b66eef6dcd05c1c130d0c75f0878241912d0abb
learningandcreativity <learningandcreativityu@gmail.com>
2023-07-20 15:02:53 -0500

    fix the branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         I am changed!
+          Learn React
         </a>
       </header>
     </div>
